517643b5684414320314b893e2fc5892aea5893b
Nicholas McMaster <nmcmaster@gmail.com>
2024-07-23 20:59:48 -0400

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
+import AlbumSquare from "./ui/AlbumSquare";
 
 export default function Home() {
 	return (
@@ -7,19 +8,7 @@ export default function Home() {
 			<div className="bg-stone-200 min-h-screen">
 				<h1 className="text-center text-6xl pt-6 pb-10">Edenic Past</h1>
 				<div className="flex flex-wrap justify-around">
-					<div className="w-2/5">
-						<Image
-							src="/red-amarcord-square.png"
-							className="block shadow-lg"
-							width={1000}
-							height={1000}
-							alt="Red Amarcord Cover"
-						/>
-						<div className="font-semibold text-center text-lg py-2">
-							Red Amarcord
-						</div>
-					</div>
-					<div className="w-2/5">
+					<AlbumSquare title="Two-song promo">
 						<Image
 							src="/two-song-promo.jpg"
 							className="block shadow-lg"
@@ -27,10 +16,41 @@ export default function Home() {
 							height={1000}
 							alt="Edenic Past two song promo cover"
 						/>
-						<div className="font-semibold text-center text-lg py-2">Two-song promo</div>
-					</div>
+					</AlbumSquare>
+					<AlbumSquare title="Red Amarcord">
+						<Image
+							src="/red-amarcord-square.png"
+							className="block shadow-lg"
+							width={1000}
+							height={1000}
+							alt="Red Amarcord Cover"
+						/>
+					</AlbumSquare>
 				</div>
-				<p className="border-t w-4/5 pt-3 px-3 mt-4 text-md mx-auto leading-6 border-black">Edenic Past was founded in May 2020 by Nicholas McMaster to explore political and personal themes through a studio-only brutal death metal band with programmed drums. Handling said drums, bass, and song structures himself, he asked long-time collaborators and friends Colin Marston and Paulo Henri Paguntalan to write and play guitar and vocals, respectively. Marston has also produced, engineered, and mixed every recording. The views expressed on this website in exposition of the themes for Edenic Past's songs should be taken as McMaster's views alone, not necessarily speaking for the other members.</p>
+				<div className="border-t indent-3 w-4/5 py-3 px-3 mt-4 text-md mx-auto leading-6 border-black space-y-2">
+					<p>
+						Edenic Past was founded in May 2020 by Nicholas McMaster
+						to explore political and personal themes through a
+						studio-only brutal death metal band with programmed
+						drums. Handling said drums, bass, and song structures
+						himself, he asked long-time collaborators and friends
+						Colin Marston and Paulo Henri Paguntalan to write and
+						play guitar and vocals, respectively. Marston has also
+						produced, engineered, and mixed every recording. The
+						views expressed on this website in exposition of the
+						themes for Edenic Past's songs should be taken as
+						McMaster's views alone, not necessarily speaking for the
+						other members.
+					</p>
+					<p>
+						The name Edenic Past refers to refers to a core element
+						of fascism: the appeal to an imaginary before-time which
+						is nebulously defined but was better than the present
+						(more often referred to in political writings as the
+						"mythic past").
+					</p>
+					<p>Edenic Past's vocals do not contain lyrics.</p>
+				</div>
 			</div>
 		</main>
 	);
--- src/app/ui/AlbumSquare.tsx
+++ src/app/ui/AlbumSquare.tsx
@@ -1,11 +1,11 @@
 import { ReactNode } from "react";
 
-export default function AlbumSquare({ children }: { children: ReactNode }) {
+export default function AlbumSquare({ children, title }: { children: ReactNode, title: string }) {
 	return (
 		<div className="w-2/5">
 			{children}
 			<div className="font-semibold text-center text-lg py-2">
-				Red Amarcord
+				{title}
 			</div>
 		</div>
 	);
