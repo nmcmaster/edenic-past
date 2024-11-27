88044b42c0e81bb6735b47b5ec82d916ec527eb0
Nicholas McMaster <nmcmaster@gmail.com>
2024-11-27 14:15:16 -0500

    this

--- src/app/utils/constants.tsx
+++ src/app/utils/constants.tsx
@@ -11,4 +11,4 @@ export const paragraphBottomMargin = 'mb-5';
 
 export const playerCss = "lg:w-1/3 w-full mx-auto";
 
-export const pullQuote = "mx-auto text-left sm:text-center mb-7 sm:w-4/5 w-full px-2 sm:px-0";
+export const pullQuote = "mx-auto text-left sm:text-center mb-7 sm:w-4/5 w-full px-4 sm:px-0";
